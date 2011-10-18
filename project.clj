3eadfca3766fdb1ec5d67aa0e9b43133a8404ee4
Max Penet <max.penet@gmail.com>
2011-10-18 11:52:08 +0200

    add test for latest bug discovery

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject bug-playground "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [ring/ring "0.3.7"]
-                 [aleph "0.2.0-alpha2"]])
+                 [ring/ring "0.3.8"]
+                 [aleph "0.2.0-beta2"]])
---
+++ src/bug_playground/twstream.clj
@@ -0,0 +1,21 @@
+(ns bug-playground.twstream
+  (use [lamina core]
+       [aleph http formats]))
+
+
+(def works "follow=14942630&track=APPLEPIENOGOOD,bieber")
+(def breaks "follow=14942630&track=APPLEPIENOGOOD")
+
+(defn -main
+  [& args]
+  (let [ch (:body
+            (sync-http-request
+             {:method :post
+              :basic-auth ["aleph_example" "_password"]
+              :headers {"content-type" "application/x-www-form-urlencoded"}
+              :url "https://stream.twitter.com/1/statuses/filter.json"
+              :body works
+              :delimiters ["\r"]}))]
+
+    (doseq [tweet (map decode-json (lazy-channel-seq ch))]
+      (println tweet))))
